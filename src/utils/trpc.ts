3b2b28c391e7684dbbe093f398dbd30aff4fecef
MedvidKYIV1997 <burdakigor@gmail.com>
2022-10-27 15:23:33 +0300

    added vercel support in trpc

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,9 +8,9 @@ function getBaseUrl() {
     //     // browser should use relative path
     //     return '';
 
-    // if (process.env.VERCEL_URL)
-    //     // reference for vercel.com
-    //     return `https://${process.env.VERCEL_URL}`;
+    if (process.env.VERCEL_URL)
+        // reference for vercel.com
+        return `https://${process.env.VERCEL_URL}`;
 
     // if (process.env.RENDER_INTERNAL_HOSTNAME)
     //     // reference for render.com
