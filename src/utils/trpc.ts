dd9132484c2f85f659ba3aa1e6f3ff569b41919e
MedvidKYIV1997 <burdakigor@gmail.com>
2022-10-27 15:46:45 +0300

    added next vercel url

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,9 +8,9 @@ function getBaseUrl() {
     //     // browser should use relative path
     //     return '';
 
-    if (process.env.VERCEL_URL)
+    if (process.env.NEXT_PUBLIC_VERCEL_URL)
         // reference for vercel.com
-        return `https://${process.env.VERCEL_URL}`;
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
     // if (process.env.RENDER_INTERNAL_HOSTNAME)
     //     // reference for render.com
