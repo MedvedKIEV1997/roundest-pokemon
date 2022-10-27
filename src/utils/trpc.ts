50d39ab44a7ce73d1a8f072316cdc99747acf369
MedvidKYIV1997 <burdakigor@gmail.com>
2022-10-27 15:58:08 +0300

    keep trying

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -4,19 +4,14 @@ import type { AppRouter } from '@/server/routers';
 import { inferProcedureOutput } from '@trpc/server';
 
 function getBaseUrl() {
-    // if (typeof window !== 'undefined')
-    //     // browser should use relative path
-    //     return '';
+    if (typeof window !== 'undefined')
+        //     // browser should use relative path
+        return '';
 
-    if (process.env.NEXT_PUBLIC_VERCEL_URL)
+    if (process.env.VERCEL_URL)
         // reference for vercel.com
-        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+        return `https://${process.env.VERCEL_URL}`;
 
-    // if (process.env.RENDER_INTERNAL_HOSTNAME)
-    //     // reference for render.com
-    //     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-
-    // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
